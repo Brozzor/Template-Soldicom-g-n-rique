adcfc41586d4fabdd9ca5fd2bb1ec423963944a0
Brozzor <377roro@gmail.com>
2020-04-21 10:43:07 +0200

    style (fo) ajout de la page livraison et des horaires sur la pages d'acceuil

--- js/main.js
+++ js/main.js
@@ -4,17 +4,14 @@
 let background = {
     "bgImg": [
         "two-burgers.jpg", 
-        "ognon-burger.jpg", 
         "multiple-burgers.jpg"
     ],
     "bgTitle": [
         "Big Lorem Ipsum", 
-        "Dolor Sit Amet", 
         "Simple lorem ipsum"
     ],
     "bgtxt": [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", 
         "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit"
     ]
 }
